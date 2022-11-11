5a0b3c4caf5caafae07eac5f79e5f04315b4752f
melhdzn <115022707+melhdzn@users.noreply.github.com>
2022-11-10 22:38:20 -0600

    planificador de gastos
    proyecto final react

---
+++ src/components/gasto.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import {
+    LeadingActions,
+    SwipeableList,
+    SwipeableListItem,
+    SwipeAction,
+    TrailingActions
+} from 'react-swipeable-list'
+import "react-swipeable-list/dist/styles.css"
+
+import { formatearFecha } from '../helpers'
+
+import IconoAhorro from '../img/icono_ahorro.svg'
+import IconoCasa from '../img/icono_casa.svg'
+import IconoComida from '../img/icono_comida.svg'
+import IconoGastos from '../img/icono_gasto.svg'
+import IconoDiversion from '../img/icono_diversion.svg'
+import IconoSalud from '../img/icono_salud.svg'
+import IconoMascotas from '../img/icono_Mascotas.svg'
+
+
+const diccionarioIconos = {
+
+    ahorro : IconoAhorro,
+    comida : IconoComida,
+    casa : IconoCasa,
+    gastos : IconoGastos,
+    diversion : IconoDiversion,
+    salud : IconoSalud,
+    mascotas : IconoMascotas,
+}
+
+const Gasto = ({gasto, setGastoEditar, eliminarGasto}) => {
+    const { categoria, nombre, cantidad, id, fecha } = gasto;
+
+    const leadingActions = () => (
+        <LeadingActions>
+            <SwipeAction onClick={() => setGastoEditar(gasto)}>
+                Editar
+            </SwipeAction>
+        </LeadingActions>
+    )
+
+    const trailingActions = () => (
+        <TrailingActions>
+            <SwipeAction 
+                onClick={() => eliminarGasto(id)}
+                destructive={true}
+            >
+                Eliminar
+            </SwipeAction>
+        </TrailingActions>
+    )
+
+    return (
+        <SwipeableList>
+            <SwipeableListItem
+                leadingActions={leadingActions()}
+                trailingActions={trailingActions()}
+            >
+                <div className="gasto sombra">
+                    <div className="contenido-gasto">
+                        <img 
+                            src={diccionarioIconos[categoria]}
+                            alt="Icono Gasto"
+                        />
+                        <div className="descripcion-gasto">
+                            <p className="categoria">{categoria}</p>
+                            <p className="nombre-gasto">{nombre}</p>
+                            <p className="fecha-gasto">
+                                Agregado el: {''}
+                                <span>{formatearFecha(fecha)}</span>
+                            </p>
+                        </div>
+                    </div>
+                    <p className="cantidad-gasto">${cantidad}</p>
+                </div>
+            </SwipeableListItem>
+        </SwipeableList>
+    )
+}
+
+export default Gasto
---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["index.html", "./src/componentes/*.jsx"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
